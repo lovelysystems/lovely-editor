2d7492b62fbad9c44ef2c563e6d4831cfd05fc59
Stefan Natter <stefan@natter.at>
2017-10-19 16:56:26 +0200

    added build process (dist folder)

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { Editor } from './components/editor'
+export { EditorBlock } from './components/editor-block'
+export { ExampleImage } from './components/example-image'
+export { ExampleInput } from './components/example-input'
