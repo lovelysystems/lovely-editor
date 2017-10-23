0de57ac59dbdbdecfcc3b2ba46e420eb32107bbd
Florian Hämmerle <haemmerle@lovelysystems.com>
2017-10-23 08:45:03 +0200

    add some tests for editor-block

--- src/components/editor-block/tests/index.spec.js
+++ src/components/editor-block/tests/index.spec.js
@@ -3,12 +3,38 @@ import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import { EditorBlock } from '../'
 
+const block = {
+  meta: {
+    title: 'Example Block',
+  },
+}
+
 describe('<EditorBlock />', () => {
 
   it('component renders', () => {
     const wrapper = shallow(
-      <EditorBlock />
+      <EditorBlock block={block}>
+        <div />
+      </EditorBlock>
     )
     expect(wrapper.exists()).to.equal(true)
+  })
+
+  it('renders the block title', () => {
+    const wrapper = shallow(
+      <EditorBlock block={block}>
+        <div />
+      </EditorBlock>
+    )
+    expect(wrapper.find('.oy-editor-block__title').text()).to.equal(block.meta.title)
+  })
+
+  it('renders the child element', () => {
+    const wrapper = shallow(
+      <EditorBlock block={block}>
+        <div id='child' />
+      </EditorBlock>
+    )
+    expect(wrapper.find('#child').exists()).to.equal(true, '#child not found')
   })
 })
