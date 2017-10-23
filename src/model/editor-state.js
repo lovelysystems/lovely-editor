8710f62e954606402a00f11d7d1eb65aa9a2d944
Florian Hämmerle <haemmerle@lovelysystems.com>
2017-10-23 15:21:53 +0200

    add editor-state documentation

--- src/model/editor-state.js
+++ src/model/editor-state.js
@@ -1,11 +1,52 @@
 import { filter, find, map } from 'lodash'
 
+/**
+ * @typedef {ContentBlock[]} EditorState
+ *
+ * @typedef {Object} ContentBlock
+ *  @property {string} id
+ *  @property {ContentBlockData} data
+ *  @property {ContentBlockMeta} meta
+ *
+ * @typedef {Object} ContentBlockData
+ *
+ * @typedef {Object} ContentBlockMeta
+ *  @property {string} title
+ */
+
+/**
+ * Returns the block identified by `blockId` or undefined if `editorState`
+ * contains no such block.
+ *
+ * @param {EditorState} editorState
+ * @param {string} blockId
+ *
+ * @returns [ContentBlock]
+ */
 const findBlock = (editorState, blockId) =>
   find(editorState, ['id', blockId])
 
+/**
+ * Creates a new EditorState with the specified block removed.
+ *
+ * @param {EditorState} editorState
+ * @param {string} blockId
+ *
+ * @returns {EditorState}
+ */
 const removeBlock = (editorState, blockId) =>
   filter(editorState, b => b.id !== blockId)
 
+/**
+ * Creates a new EditorState with the specified block's data updated to the
+ * specified data.
+ *
+ * @param {EditorState} editorState
+ * @param {string} blockId
+ * @param {ContentBlockData} data
+ *
+ * @returns {EditorState}
+ */
 const updateBlockData = (editorState, blockId, data) =>
   map(editorState, block => {
     if (block.id === blockId) {
@@ -17,6 +58,14 @@ const updateBlockData = (editorState, blockId, data) =>
     return block
   })
 
+/**
+ * Creates a new EditorState with the specified block added to the end.
+ *
+ * @param {EditorState} editorState
+ * @param {ContentBlock} block
+ *
+ * @returns {EditorState}
+ */
 const appendBlock = (editorState, block) =>
   [...editorState, block]
 
