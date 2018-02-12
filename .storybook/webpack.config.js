073f44119761dddbe59dd72d8ea5512d84526905
natterstefan <stefan@natter.at>
2018-02-12 08:58:46 +0100

    devPackage and storybook update

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,10 +4,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.md$/,
-        use: 'raw-loader'
-      },
-      {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'],
       }
